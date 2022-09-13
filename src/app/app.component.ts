5f416b642766fb51bdbd0ad69cc06b5ad64bdbfd
RuthLeska <ruth_leska@web.de>
2022-09-13 10:36:20 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from "./services/authentication.service";
+import { User } from "./models/user.model";
+import { Router } from "@angular/router";
+import { Role } from "./models/role.num";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'angular-book-seller';
+
+  currentUser: User = new User();
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe( data => {
+      this.currentUser = data;
+    })
+  }
+
+  isAdmin() {
+    return this.currentUser?.role ===  Role.ADMIN;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
